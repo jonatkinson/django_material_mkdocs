0551839c74a286e1d8f8a7d5b04e8db531c8c62c
Jon Atkinson <jon@wearefarm.com>
2018-07-28 15:40:41 +0100

    Added binary detection to the serve view.

--- docs/views.py
+++ docs/views.py
@@ -1,8 +1,9 @@
 import os
 import mimetypes
 
-from django.conf import settings
+from binaryornot.check import is_binary
 
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.staticfiles.views import serve
 from django.http import HttpResponse
@@ -12,8 +13,12 @@ def serve_docs(request, path):
     file_path = os.path.join(settings.BASE_DIR, "mkdocs_build", path)
     file_mime = mimetypes.guess_type(file_path)
 
-    with open(file_path, 'r') as file_handle:
-        data = file_handle.read()
+    if is_binary(file_path):
+        with open(file_path, 'rb' as file_handle:
+            data = file_handle.read()
+    else:
+        with open(file_path, 'r') as file_handle:
+            data = file_handle.read()
    
     response = HttpResponse(content_type=file_mime)
     response.write(data)
